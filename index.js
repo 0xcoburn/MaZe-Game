feb96c7514e0afbd1b5e0673e69ca2b06f9dc982
Matt Coburn <mattcoburn24@gmail.com>
2020-03-15 00:01:05 -0700

    array of shuffled neighboring cells
    now have a function to shuffle an array of neighboring cells that will help generate the random maze

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
 const width = 600;
 const height = 600;
 
@@ -37,6 +38,44 @@ World.add(world, walls);
 
 // MaZe Generation
 
-const grid = Array(3).fill(null).map(() => Array(3).fill(false));
+const shuffle = (arr) => {
+	let counter = arr.length;
+	while (counter > 0) {
+		const index = Math.floor(Math.random() * counter);
+		counter--;
+		const temp = arr[counter];
+		arr[counter] = arr[index];
+		arr[index] = temp;
+	}
+	return arr;
+};
 
-console.log(grid);
+const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
+
+const verticles = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
+const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startCol = Math.floor(Math.random() * cells);
+
+stepThruCell = (row, col) => {
+	//If i have visited the cell at {row, column}, then return
+	if (grid[row][col] === true) {
+		return;
+	}
+	//Mark this cell as being visited
+	grid[row][col] = true;
+	//Assemble randomly-ordered list of neighbors
+	const neighbors = shuffle([ [ row - 1, col ], [ row + 1, col ], [ row, col - 1 ], [ row, col + 1 ] ]);
+	console.log(neighbors);
+	//For each neighbor...
+
+	//see if that neighbor is out of bounds
+
+	//if we have visited that neighbor continue to next neighbor
+
+	//remove wall from either horizontal or verticle array
+
+	// visit next cell
+};
+stepThruCell(1, 1);
